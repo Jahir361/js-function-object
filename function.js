23bf47c5d49ff2cc8da1a8de81a6ecc1eb4b61d6
jahir361 <jahiruddin361@gmail.com>
2022-08-06 22:23:26 +0600

    functin js warke process

---
+++ function.js
@@ -0,0 +1,12 @@
+// function declaration
+function getJob (){
+    console.log('To get gob in the google');
+    console.log('you have to learn more ');
+    console.log('and more and more more practice you need');
+}
+getJob();
+console.log('then you have to learn data base and algorithm');
+getJob();
+console.log('now make a project for display it .');
+getJob();
+console.log('make a CV and send to the company');
